b56ec72abd6eb786bdb54414b5e0fd962a2ed098
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-12-12 12:52:24 -0300

    feat: add hooks to queries and mutations

--- src/actions/decrease-cart-quantity/index.ts
+++ src/actions/decrease-cart-quantity/index.ts
@@ -14,29 +14,29 @@ export const decreaseCartProductQuantity = async (
   data: z.infer<typeof decreaseCartProductQuantitySchema>,
 ) => {
   decreaseCartProductQuantitySchema.parse(data);
-
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
-  if (!session?.user) throw new Error("Unauthorized");
-
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
   const cartItem = await db.query.cartItemTable.findFirst({
     where: (cartItem, { eq }) => eq(cartItem.id, data.cartItemId),
     with: {
       cart: true,
     },
   });
-
-  if (!cartItem) throw new Error("Product variant not found in cart");
-
-  if (cartItem.cart.userId !== session.user.id) throw new Error("Unauthorized");
-
+  if (!cartItem) {
+    throw new Error("Cart item not found");
+  }
+  const cartDoesNotBelongToUser = cartItem.cart.userId !== session.user.id;
+  if (cartDoesNotBelongToUser) {
+    throw new Error("Unauthorized");
+  }
   if (cartItem.quantity === 1) {
     await db.delete(cartItemTable).where(eq(cartItemTable.id, cartItem.id));
     return;
   }
-
   await db
     .update(cartItemTable)
     .set({ quantity: cartItem.quantity - 1 })
